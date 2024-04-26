18e81955276d30e29a81de724fd263aecf2bb39b
sarbajit1000 <you@example.com>
2024-04-26 17:31:39 +0530

    css added

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("<int:month>", views.month_challenges_by_number),
     path("<str:month>", views.month_challenges, name="month-challenges"),
     
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 # Create your views here.
@@ -16,24 +16,20 @@ month_challenge = {
     "sep":"learn Django everyday",
     "oct":"drink 5 liter every day",
     "nov":"learn Django 20 min a day",
-    "dec":"learn Django everyday",
+    "dec":None,
 }
 
-def index(request,):
-    list_items = ""
+def index(request):
     months = list(month_challenge.keys())
-    for month in months:
-        capitalize_month = month.capitalize()
-        month_path = reverse("month-challenges", args=[month])
-        list_items += f"<h1><li><a href=\"{month_path}\">{capitalize_month}</a></li></h1>"
-        response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
 
+    return render (request, "challenges/index.html", {
+        "months": months
+    })
 
 def month_challenges_by_number(request, month):
     months = list(month_challenge.keys())
     if month > len(months):
-        return HttpResponse("Not A Valid Month")
+        return HttpResponseNotFound("Not A Valid Month")
     redirect_month = months[month-1]
     redirect_path = reverse("month-challenges", args=[redirect_month])
     return HttpResponseRedirect(redirect_path)
@@ -48,5 +44,5 @@ def month_challenges(request, month):
             "month":month                                         
         })
     except:
-        return HttpResponseNotFound("Month is not valid")
+        raise Http404()
 
